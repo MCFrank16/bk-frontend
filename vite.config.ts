40be79ad6a738dd9a07a0cc8b127e04bc1a5b21b
Frank Mutabazi <frank@Franks-MacBook-Pro.local>
2025-02-05 21:57:53 +0200

    check url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: 'https://bk-frontend-production.up.railway.app/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 });
 
