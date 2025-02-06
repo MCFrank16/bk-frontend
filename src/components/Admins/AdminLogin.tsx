0920bf775b44dc824961eaf388817134b13c01fd
Frank Mutabazi <frank@Franks-MacBook-Pro.local>
2025-02-06 11:35:21 +0200

    add email and password for ease

--- src/components/Admins/AdminLogin.tsx
+++ src/components/Admins/AdminLogin.tsx
@@ -8,131 +8,145 @@ import useAuth from '../../hooks/useAuth';
 import { LoginAdmin } from '../../backend/api';
 
 interface FormValues {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 
 const AdminLogin: FC = () => {
 
-  const { user: { setUser} } = useAuth();
+    const { user: { setUser } } = useAuth();
 
-  const navigate = useNavigate();
-  const [error, setError] = useState("");
+    const navigate = useNavigate();
+    const [error, setError] = useState("");
 
-  const initialValues: FormValues = {
-      email: '',
-      password: '',
-  };
+    const initialValues: FormValues = {
+        email: '',
+        password: '',
+    };
 
-  const validationSchema = Yup.object({
-      email: Yup.string()
-          .email('Invalid email address')
-          .required('Email is required'),
-      password: Yup.string().required('Password is required'),
-  });
+    const validationSchema = Yup.object({
+        email: Yup.string()
+            .email('Invalid email address')
+            .required('Email is required'),
+        password: Yup.string().required('Password is required'),
+    });
 
-  const { mutateAsync, isPending, isError } = useMutation({
-      mutationFn: async (values: any) => {
-          const body = {
-              email: values["email"],
-              password: values["password"]
-          };
+    const { mutateAsync, isPending, isError } = useMutation({
+        mutationFn: async (values: any) => {
+            const body = {
+                email: values["email"],
+                password: values["password"]
+            };
 
-          return await LoginAdmin(body);
-      },
-      retry: false,
-      onSettled: (data: any, error: any) => {
-          if (!error) {
-              setError("");
-              setUser({ ...data.data.data, type: 'admin' });
-              localStorage.setItem('user', JSON.stringify({ ...data.data.data, type: 'admin' }));
-              navigate('/admin');
-          } else {
-              setError(error?.response ? `${error.response.data.message}` : error?.message);
-          }
-      }
-  })
+            return await LoginAdmin(body);
+        },
+        retry: false,
+        onSettled: (data: any, error: any) => {
+            if (!error) {
+                setError("");
+                setUser({ ...data.data.data, type: 'admin' });
+                localStorage.setItem('user', JSON.stringify({ ...data.data.data, type: 'admin' }));
+                navigate('/admin');
+            } else {
+                setError(error?.response ? `${error.response.data.message}` : error?.message);
+            }
+        }
+    })
 
-  return (
-    <div className='h-full flex flex-col'>
+    return (
+        <div className='h-full flex flex-col'>
 
-        <div className='flex-grow flex justify-center items-center'>
+            <div className='flex-grow flex justify-center items-center'>
 
-            <Formik
-                initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={async (values) => {
-                    setError("");
-                    await mutateAsync(values);
-                }}
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={async (values) => {
+                        setError("");
+                        await mutateAsync(values);
+                    }}
 
-            >
+                >
 
-                {() => {
-                    return (
-                        <Form className='w-1/3 flex flex-col'>
+                    {() => {
+                        return (
+                            <Form className='w-1/3 flex flex-col'>
 
-                            { isError && <div className='flex justify-center items-center text-red-500 mb-2'>
-                                {error}
-                            </div>}
+                                <div className='flex mb-3 justify-between items-center'>
+                                    <div className='flex space-x-2'>
+                                        <span className='font-bold'>Email:</span>
+                                        <span>admin@gmail.com</span>
+                                    </div>
 
-                            <div className='bg-white p-2'>
-                                <div className='flex flex-col mb-3'>
-                                    <span className='mb-2 font-bold'>Email</span>
-                                    <Field
-                                        name="email"
-                                        className="bg-gray-100 p-2 rounded outline-none"
-                                        placeholder="email"
-                                    />
-
-                                    <ErrorMessage name='email' render={msg => <span className='text-red-500 text-xs'>{msg}</span>} />
+                                    <div className='flex space-x-2'>
+                                        <span className='font-bold'>Password:</span>
+                                        <span>admin@123</span>
+                                    </div>
 
                                 </div>
 
-                                <div className='flex flex-col mb-5'>
-                                    <span className='mb-2 font-bold'>Password</span>
-                                    <Field
-                                        name="password"
-                                        type='password'
-                                        className="bg-gray-100 p-2 rounded outline-none"
-                                        placeholder="password"
-                                    />
+                                <div className='bg-white p-2'>
 
-                                    <ErrorMessage name='password' render={msg => <span className='text-red-500 text-xs'>{msg}</span>} />
+                                    {isError && <div className='flex justify-center items-center text-red-500 mb-2'>
+                                        {error}
+                                    </div>}
 
+                                    <div className='flex flex-col mb-3'>
+                                        <span className='mb-2 font-bold'>Email</span>
+                                        <Field
+                                            name="email"
+                                            className="bg-gray-100 p-2 rounded outline-none"
+                                            placeholder="email"
+                                        />
+
+                                        <ErrorMessage name='email' render={msg => <span className='text-red-500 text-xs'>{msg}</span>} />
+
+                                    </div>
+
+                                    <div className='flex flex-col mb-5'>
+                                        <span className='mb-2 font-bold'>Password</span>
+                                        <Field
+                                            name="password"
+                                            type='password'
+                                            className="bg-gray-100 p-2 rounded outline-none"
+                                            placeholder="password"
+                                        />
+
+                                        <ErrorMessage name='password' render={msg => <span className='text-red-500 text-xs'>{msg}</span>} />
+
+                                    </div>
+
+                                    <div className='flex justify-center items-center'>
+                                        <button
+                                            type="submit"
+                                            className='secondary-bg-color px-8 py-2 text-white rounded cursor-pointer'
+                                        >
+
+                                            {
+                                                isPending ? 'Loading...' : 'Login'
+                                            }
+
+                                        </button>
+                                    </div>
                                 </div>
 
-                                <div className='flex justify-center items-center'>
-                                    <button
-                                        type="submit"
-                                        className='secondary-bg-color px-8 py-2 text-white rounded cursor-pointer'
-                                    >
-
-                                        {
-                                            isPending ? 'Loading...' : 'Login'
-                                        }
-
-                                    </button>
-                                </div>
-                            </div>
 
 
+                            </Form>
+                        )
+                    }}
 
-                        </Form>
-                    )
-                }}
+                </Formik>
 
-            </Formik>
+                <div >
 
-            <div >
+                </div>
 
             </div>
-
+            <Footer />
         </div>
-        <Footer />
-    </div>
-)
+    )
 }
 
 export default AdminLogin
--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 4173, // Or use process.env.PORT if required
-  },
-  preview: {
-    host: 'bk-frontend-production.up.railway.app',
-    port: 4173,
-  },
   plugins: [react(), tailwindcss()],
 });
-
